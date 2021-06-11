35dc79e63b28da045fb7d821f2053745d8bb4b13
Yusuf Ekiz <yusuf.ekiz@check24.de>
2021-06-11 21:27:43 +0200

    * Init base MVVM components

---
+++ app/src/main/java/com/ekiz/sixtcodingtask/base/BaseActivity.kt
@@ -0,0 +1,28 @@
+package com.ekiz.sixtcodingtask.base
+
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import dagger.android.support.DaggerAppCompatActivity
+import java.lang.reflect.ParameterizedType
+import javax.inject.Inject
+
+abstract class BaseActivity<VM : BaseViewModel, B : androidx.databinding.ViewDataBinding> : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    @Suppress("UNCHECKED_CAST")
+    protected val viewModel by lazy (LazyThreadSafetyMode.NONE) {
+        val persistentViewModelClass = (javaClass.genericSuperclass as ParameterizedType)
+            .actualTypeArguments[0] as Class<VM>
+        return@lazy ViewModelProviders.of(this, viewModelFactory).get(persistentViewModelClass)
+    }
+
+    protected val binder by lazy<B>(LazyThreadSafetyMode.NONE) {
+        return@lazy DataBindingUtil.setContentView(this, layoutId())
+    }
+
+    abstract fun layoutId(): Int
+}
