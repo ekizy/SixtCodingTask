cce5d8fcc0ca93f7b522595428eafc469d8f8566
Yusuf Ekiz <yusuf.ekiz@check24.de>
2021-06-12 18:22:05 +0200

    * Implement maps api

--- app/src/main/java/com/ekiz/sixtcodingtask/scenes/carsoverview/CarsOverviewFragment.kt
+++ app/src/main/java/com/ekiz/sixtcodingtask/scenes/carsoverview/CarsOverviewFragment.kt
@@ -1,26 +1,39 @@
 package com.ekiz.sixtcodingtask.scenes.carsoverview
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.ekiz.sixtcodingtask.R
 import com.ekiz.sixtcodingtask.base.BaseFragment
 import com.ekiz.sixtcodingtask.databinding.FragmentCarsOverviewBinding
+import com.ekiz.sixtcodingtask.util.Constants.MAPVIEW_BUNDLE_KEY
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class CarsOverviewFragment : BaseFragment<CarsOverviewViewModel, FragmentCarsOverviewBinding>() {
+class CarsOverviewFragment : BaseFragment<CarsOverviewViewModel, FragmentCarsOverviewBinding>(){
 
     override fun layoutId(): Int = R.layout.fragment_cars_overview
 
-    override fun initialize() {
-        print("asdsds")
+    override fun initialize(savedInstanceState: Bundle?) {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = CarsOverviewFragment()
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
 }
---
+++ app/src/main/java/com/ekiz/sixtcodingtask/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.ekiz.sixtcodingtask.util
+
+object Constants {
+    const val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+}
